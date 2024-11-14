bcf28f99325af4ea1fe54001a85556da2aa52b98
fidelisobioma <mbamfidelisobioma1@gmail.com>
2024-11-14 19:47:16 +0100

    added form validtion

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const email = document.querySelector("#email");
 const postalCode = document.querySelector("#postalcode");
-const postMessage = document.querySelector("#postmessage");
+const postmessage = document.querySelector("#postmessage");
 const password = document.querySelector("#password");
 const passMessage = document.querySelector("#passmessage");
 const confirmPassword = document.querySelector("#confirmpassword");
 const confmessage = document.querySelector("#confmessage");
-console.log(confmessage);
+const success = document.querySelector(".success");
+console.log(success);
 const emailMessage = document.querySelector("#emailmessage");
 const submit = document.querySelector(".submit");
 submit.addEventListener("click", (e) => {
@@ -18,21 +19,35 @@ submit.addEventListener("click", (e) => {
   }
 
   if (!postalCode.checkValidity()) {
-    postMessage.textContent = postalCode.validationMessage;
+    postmessage.textContent = postalCode.validationMessage;
   } else {
-    postMessage.textContent = "";
+    postmessage.textContent = "";
   }
 
   if (!password.checkValidity()) {
     passMessage.textContent = password.validationMessage;
   } else {
-    passMessage.textContent = "";
+    passmessage.textContent = "";
   }
 
   if (confirmPassword.value !== password.value) {
     confmessage.textContent = "password does not match";
   } else {
     confmessage.textContent = "";
+  }
+  //add success message
+  if (
+    emailMessage.textContent == "" &&
+    postmessage.textContent == "" &&
+    passMessage.textContent == "" &&
+    confmessage.textContent == ""
+  ) {
+    setTimeout(() => {
+      success.classList.add("showsuccess");
+    }, 1000);
+    setTimeout(() => {
+      location.reload();
+    }, 5000);
   }
 });
 // console.log(emailMessage);
